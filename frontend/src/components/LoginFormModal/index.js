7db98cc69e08731d0dbb9edae69d267961c8a38c
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-02-21 00:29:58 -0500

    added styles to login modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -26,32 +26,37 @@ function LoginFormModal() {
 
   return (
     <>
-      <h1>Log In</h1>
+      {/* <h1>Log In</h1> */}
       <form onSubmit={handleSubmit}>
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
+        <div className="login-form-container">
+          <div className="login-cred-container">
+              <input
+                type="text"
+                placeholder="Your username or email"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+          </div>
+          <div className="login-pw-container">
+              <input
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+          </div>
+          <button type="submit">Continue</button>
+          <div className="login-disclaimer">
+            <p>When registering, you agree that we may use your provided data for the registration and to send you notifications on our products and services. You can unsubscribe from notifications at any time in your settings. For additional info please refer to our Privacy Policy.</p>
+          </div>
+        </div>
       </form>
     </>
   );
