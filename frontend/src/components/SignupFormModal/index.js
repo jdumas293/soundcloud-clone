be8e4c432e11dd667e653cc7b24fd60363f8ed74
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-02-21 00:57:31 -0500

    added styles to signup modal

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -29,48 +29,53 @@ function SignupFormModal() {
 
   return (
     <>
-      <h1>Sign Up</h1>
+      {/* <h1>Sign Up</h1> */}
       <form onSubmit={handleSubmit}>
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
+        <div className="signup-form-container">
+          <div className="signup-email-container">
+              <input
+                type="text"
+                placeholder="Your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+          </div>
+          <div className="signup-username-container">
+              <input
+                type="text"
+                placeholder="Your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+          </div>
+          <div className="signup-pw-container">
+              <input
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+          </div>
+          <div className="signup-confirm-pw">
+              <input
+                type="password"
+                placeholder="Please confirm your password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+          </div>
+          <button type="submit">Continue</button>
+          <div className="login-disclaimer">
+            <p>When registering, you agree that we may use your provided data for the registration and to send you notifications on our products and services. You can unsubscribe from notifications at any time in your settings. For additional info please refer to our Privacy Policy.</p>
+          </div>
+        </div>
       </form>
     </>
   );
